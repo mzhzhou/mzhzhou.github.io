f8ad9742ec37aad8e2fc18830a3eb54902e8c0fd
mzhzhou <margaretzhou@mde.harvard.edu>
2024-05-13 16:05:57 -0400

    update

---
+++ script.js
@@ -0,0 +1,66 @@
+/*
+  This is your site JavaScript code - you can add interactivity!
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+// console.log("Hello 🌎");
+
+/* 
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the steps in the TODO 🚧
+*/
+// const btn = document.querySelector("button"); // Get the button from the page
+// if (btn) { // Detect clicks on the button
+//   btn.onclick = function () {
+//     // The 'dipped' class in style.css changes the appearance on click
+//     btn.classList.toggle("dipped");
+//   };
+// }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const circle = document.querySelector('.gradient_circle');
+    let dx = (Math.random() < 0.5 ? -1 : 1) * (Math.random() * 3 + 0.5);  // Change in position along X-axis
+    let dy = (Math.random() < 0.5 ? -1 : 1) * (Math.random() * 3 + 0.5);  // Change in position along Y-axis
+    let x = window.innerWidth / 2;
+    let y = 0;
+
+    function move() {
+        if (x + dx > window.innerWidth - 200 || x + dx < 0) {  // Check horizontal boundaries
+            dx = -dx;
+        }
+        if (y + dy > window.innerHeight - 200 || y + dy < 0) {  // Check vertical boundaries
+            dy = -dy;
+        }
+        x += dx;
+        y += dy;
+
+        circle.style.left = `${x}px`;
+        circle.style.top = `${y}px`;
+
+        requestAnimationFrame(move);
+    }
+
+    move();
+});
+
+document.addEventListener("mousemove", function (event) {
+    const circle = document.querySelector('.gradient_circle_pointer');
+    circle.style.transform = `translate3d(${event.clientX - 50}px, ${event.clientY - 50}px, 0)`;
+    // Adjust the translate3d values by subtracting half the size of the circle to center it on the cursor
+});
+
+
+// ----- GLITCH STARTER PROJECT HELPER CODE -----
+
+// Open file when the link in the preview is clicked
+let goto = (file, line) => {
+    window.parent.postMessage(
+        { type: "glitch/go-to-line", payload: { filePath: file, line: line } }, "*"
+    );
+};
+// Get the file opening button from its class name
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+    f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
+});
